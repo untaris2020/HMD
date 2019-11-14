301799a991ddd9ce35c47f64d32adc30bfdfa91d
Tim <tstern2797@gmail.com>
2019-11-14 15:51:01 -0600

    Initializing game objects

---
+++ Assets/Scripts/HID/HIDManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HIDManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject NAV;
+    private GameObject NAVInstance;
+
+    public GameObject TELEM;
+    private GameObject TELEMInstance;
+
+    public GameObject MISSION;
+    private GameObject MISSIONInstance;
+
+    void Start()
+    {
+        Debug.Log("Initializing Navagation Object...");
+        NAVInstance = Instantiate(NAV, new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
+        Debug.Log("Initializing Telem Object...");
+        TELEMInstance = Instantiate(TELEM, new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
+        Debug.Log("Initializing Mission Object...");
+        MISSIONInstance = Instantiate(MISSION, new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject HID;
+    private GameObject HIDInstance;
+    public GameObject EHS;
+    private GameObject EHSInstance; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting Scene...");
+        Debug.Log("Initializing HID...");
+        HIDInstance = Instantiate(HID, new Vector3(0, 0, 0), Quaternion.identity);
+        Debug.Log("Initializing EHS...");
+        EHSInstance = Instantiate(EHS, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
