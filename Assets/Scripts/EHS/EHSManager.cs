629279468027bd68f2a537cce9bb6001fcead5f4
Tim <tstern2797@gmail.com>
2019-12-04 01:20:25 -0600

    Some stuff i missed for last commit

---
+++ Assets/Scripts/EHS/EHSManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EHSManager : MonoBehaviour
+{
+    public GameObject IMUChestObj;
+    public GameObject IMUChest; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       IMUChest = Instantiate(IMUChestObj, new Vector3(0, 0, 0), Quaternion.identity, gameObject.transform) as GameObject;
+    }
+}
