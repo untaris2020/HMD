54fd3fc4b7c9290f528378071a6b6c225456ea75
ARIS <57504797+untaris2020@users.noreply.github.com>
2020-04-07 01:10:51 -0400

    Audio Saving in progress. Still need to load and play in scene

--- Assets/Scripts/HID/NAV/audioManager.cs
+++ Assets/Scripts/HID/NAV/audioManager.cs
@@ -7,7 +7,7 @@ public class audioManager : MonoBehaviour
 {
     public GameObject recordButton;
     public TextMeshProUGUI recordButtonText;
-    public GameObject liveObjects;
+    public GameObject playbackTime;
     public GameObject recordingIndicator; 
 
     private bool recording;
@@ -16,17 +16,18 @@ public class audioManager : MonoBehaviour
     private float startTime;
     private float prevTime;
     private delegate void functionDelegate();
-
+    private recordAudio ar;
     // Start is called before the first frame update
     void Start()
     {
         HeadTracking ht = GameObject.Find("SceneManager").GetComponent<HeadTracking>();
+        ar = GameObject.Find("SceneManager").GetComponent<recordAudio>();
         functionDelegate rec = new functionDelegate(recordingHit);    
         ht.registerCollider(recordButton.GetComponent<Collider>().name, rec);
         Time.timeScale = 1.0f;
         updateTime = 0f;
         prevTime = 0f;
-        liveObjects.SetActive(false);
+        playbackTime.SetActive(false);
         recordingIndicator.SetActive(false);
     }
 
@@ -34,41 +35,54 @@ public class audioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(recording)
+        if(ar.isMicConnected())
         {
-            updateTime = Time.time - prevTime;
-
-            if(updateTime > waitTime)
+            if(recording)
             {
-                prevTime = Time.time;
-                float timeElapsed = Time.time - startTime; 
-                string minutes = Mathf.Floor(timeElapsed/ 60).ToString("00");
-                string seconds = Mathf.Floor(timeElapsed % 60).ToString("00");
+                updateTime = Time.time - prevTime;
 
-                liveObjects.GetComponent<TextMeshProUGUI>().SetText("REC TIME: " + minutes + ":" + seconds);
-                updateTime = 0f; 
+                if(updateTime > waitTime)
+                {
+                    prevTime = Time.time;
+                    float timeElapsed = Time.time - startTime; 
+                    string minutes = Mathf.Floor(timeElapsed/ 60).ToString("00");
+                    string seconds = Mathf.Floor(timeElapsed % 60).ToString("00");
+
+                    playbackTime.GetComponent<TextMeshProUGUI>().SetText("REC TIME: " + minutes + ":" + seconds);
+                    updateTime = 0f; 
+                }
             }
+            
         }
     }
 
     void recordingHit()
     {
-        if(recording)
+        if(ar.isMicConnected())
         {
-            liveObjects.SetActive(false);
-            recordingIndicator.SetActive(false);
-            recordButtonText.SetText("START");
-            recording = false; 
+            if(recording)
+            {
+                playbackTime.SetActive(false);
+                recordingIndicator.SetActive(false);
+                recordButtonText.SetText("START");
+                recording = false;
+                ar.stopRecording();
+            }
+            else
+            {
+                playbackTime.SetActive(true); 
+                recordingIndicator.SetActive(true);
+                recordButtonText.SetText("STOP");
+                playbackTime.GetComponent<TextMeshProUGUI>().SetText("REC TIME: 00:00");
+                recording = true;
+                startTime = Time.time;
+                prevTime = Time.time;
+                ar.startRecording();
+            }
         }
         else
         {
-            liveObjects.SetActive(true); 
-            recordingIndicator.SetActive(true);
-            recordButtonText.SetText("STOP");
-            liveObjects.GetComponent<TextMeshProUGUI>().SetText("REC TIME: 00:00");
-            recording = true;
-            startTime = Time.time;
-            prevTime = Time.time;
+            Debug.Log("NO MIC");
         }
     }   
 }
---
+++ Assets/Scripts/HID/NAV/recordAudio.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recordAudio : MonoBehaviour
+{
+    public int MAX_RECORD_TIME = 300; 
+
+    private AudioSource audioSource = null;
+   
+    List<float> tempRecording = new List<float>();
+
+    List<float[]> recordedClips = new List<float[]>();
+
+    int idx; 
+
+    private int minFreq;  
+    private int maxFreq;
+
+    private bool micConnected;
+
+    public bool isMicConnected() { return micConnected; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        idx = 0;
+
+        audioSource = GetComponent<AudioSource>();
+
+        //Check if there is at least one microphone connected  
+        if (Microphone.devices.Length <= 0)
+        {
+            DebugManager.Instance.LogBoth("NO MICROPHONE DETECTED");
+            micConnected = false;
+        }
+        else
+        {
+            micConnected = true;
+            Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);
+
+            if (minFreq == 0 && maxFreq == 0)
+            {
+                maxFreq = 44100;  //Default case use 44100 Hz rate
+            }
+        }
+    }
+
+    public void startRecording()
+    {
+        if(micConnected)
+        { 
+            audioSource.clip = Microphone.Start(null, true, MAX_RECORD_TIME, maxFreq); 
+        }
+            
+    }
+    
+    public void stopRecording()
+    {
+        if(micConnected)
+        {
+            int lastTime = Microphone.GetPosition(null);
+            Microphone.End(null);
+            float[] samples = new float[audioSource.clip.samples]; 
+            audioSource.clip.GetData(samples, 0);
+            float[] ClipSamples = new float[lastTime];
+            Array.Copy(samples, ClipSamples, ClipSamples.Length - 1);
+            audioSource.clip = AudioClip.Create("playRecordClip", ClipSamples.Length, 1, 44100, false);
+            audioSource.clip.SetData(ClipSamples, 0);
+            SavWav.Save("recording" + idx, audioSource.clip);
+            idx += 1;
+        }
+    }
+
+}
