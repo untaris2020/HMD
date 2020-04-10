8d7d4b388c0e47643c0d4e6f495d43bd3eb59460
dww0085 <davidwoodward@my.unt.edu>
2020-04-10 00:13:08 -0500

    model loader and models

--- Assets/Scripts/HID/TELEM/ModelLoader.cs
+++ Assets/Scripts/HID/TELEM/ModelLoader.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using TMPro;
 
 public class ModelLoader : MonoBehaviour
 {
     // buttons
-    public GameObject clearModelsButton, misc1Button, misc2Button, upButton, downButton, upArrow;
+    public TelemPanelManager telem_panel_manager;
+    public GameObject clearModelsButton, misc1Button, misc2Button, upButton, downButton, upArrow, _camera;
     public GameObject[] modelButtons;
 
     // text
@@ -12,11 +14,18 @@ public class ModelLoader : MonoBehaviour
     public TextMeshProUGUI heading_text;
     public TextMeshProUGUI index_text;  // left
     public TextMeshProUGUI misc_text;   // right - unused
+    public TextMeshProUGUI instructions_text;
 
     // array/list of models
-    private int NUMOFMODELS = 13;
+    private int NUMOFMODELS;
     private int NUMOFPAGES;
     private int page_index = 0;
+    private int load_model;
+    private bool ready_to_load = false;
+    public GameObject[] models;
+    public GameObject glove_model;
+    private List<GameObject> loaded_models;
+
 
     private delegate void functionDelegate();
 
@@ -24,6 +33,9 @@ public class ModelLoader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        loaded_models = new List<GameObject>();
+        NUMOFMODELS = models.Length;
+        DebugManager.Instance.LogBoth(this.GetType().Name, NUMOFMODELS + " models loaded.");
         NUMOFPAGES = (int)System.Math.Ceiling((double)NUMOFMODELS / 5);
         DebugManager.Instance.LogUnityConsole("PAGES", NUMOFPAGES.ToString());
 
@@ -33,6 +45,11 @@ public class ModelLoader : MonoBehaviour
         functionDelegate tmpDelegate0 = new functionDelegate(ClearModelsButton);
         ht.registerCollider(clearModelsButton.GetComponent<Collider>().name, tmpDelegate0);
         functionDebug.Instance.registerFunction("clearModels", tmpDelegate0);
+
+        // set position button
+        tmpDelegate0 = new functionDelegate(Set3DModelPosition);
+        ht.registerCollider(misc1Button.GetComponent<Collider>().name, tmpDelegate0);
+        functionDebug.Instance.registerFunction("set3DModelPosition", tmpDelegate0);
 
         // up arrow
         tmpDelegate0 = new functionDelegate(UpArrowButton);
@@ -67,6 +84,7 @@ public class ModelLoader : MonoBehaviour
 
         UpdateUI();
         misc_text.SetText("");
+        glove_model.SetActive(false);
     }
 
     // Update is called once per frame
@@ -86,6 +104,20 @@ public class ModelLoader : MonoBehaviour
         {
             heading_text.SetText("");
             upArrow.SetActive(true);
+        }
+
+        for (int i=0; i<5; i++)
+        {
+            int tmp_index = i + (page_index * 5);
+            // TODO need to check if in bounds of array
+            if (tmp_index >=0 && tmp_index < models.Length)
+            {
+                modelTexts[i].SetText(models[tmp_index].name);
+            } else
+            {
+                modelTexts[i].SetText("");
+            }
+            
         }
     }
 
@@ -112,9 +144,59 @@ public class ModelLoader : MonoBehaviour
         }
     }
 
+    private void LoadModel(int pos)
+    {
+        load_model = pos;
+
+        // load Telem_pg_3
+        telem_panel_manager.LoadPage(3);
+        instructions_text.SetText("Align your hand with the glove and click the SET POSITION button");
+        
+
+        // Load Hand Model and have user align glove
+        Vector3 spawn_pos = _camera.transform.position;
+        spawn_pos.z += 5;
+        spawn_pos.y -= 5;
+
+        glove_model.SetActive(true);
+        glove_model.transform.position = spawn_pos;
+
+        // waiting for user to click the button...
+        ready_to_load = true;
+    }
+
     private void ClearModelsButton()
     {
+        // not sure if this will work
+        foreach (GameObject obj in loaded_models)
+        {
+            Destroy(obj);
+        }
 
+        loaded_models.Clear();
+    }
+
+    private void Set3DModelPosition()
+    {
+        if (!ready_to_load)
+        {
+            return;
+        }
+
+        // Load and scale selected model
+        if (load_model == -1)
+        {
+            DebugManager.Instance.LogBoth(this.GetType().Name, "ERROR: load_model number not set");
+        }
+        int model_index = load_model + (page_index * 5);
+
+        loaded_models.Add( (GameObject)Instantiate(models[model_index], new Vector3(0, 0, -15), Quaternion.identity));
+        DebugManager.Instance.LogBoth("INFO", "Loading 3D Model " + models[model_index].name);
+
+        load_model = -1;
+        glove_model.SetActive(false);
+        instructions_text.SetText("");
+        ready_to_load = false;
     }
 
     private void UpArrowButton()
@@ -129,26 +211,26 @@ public class ModelLoader : MonoBehaviour
 
     private void LoadModel0()
     {
-
+        LoadModel(0);
     }
 
     private void LoadModel1()
     {
-
+        LoadModel(1);
     }
 
     private void LoadModel2()
     {
-
+        LoadModel(2);
     }
 
     private void LoadModel3()
     {
-
+        LoadModel(3);
     }
 
     private void LoadModel4()
     {
-
+        LoadModel(4);
     }
 }
